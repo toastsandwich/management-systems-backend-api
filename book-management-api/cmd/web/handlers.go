9e9ef275f92534d505594c0b93f9197f42df1f50
toastsandwich <shreyasxcore2003@gmail.com>
2024-05-21 07:25:18 +0530

    moved handler.go

---
+++ book-management-api/cmd/web/handlers.go
@@ -0,0 +1,13 @@
+package web
+
+import (
+	"net/http"
+)
+
+func (a *Application) Home(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (a *Application) CreateBook(w http.ResponseWriter, r *http.Request) {
+
+}
