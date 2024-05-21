4243c8421a1258e4f73f5dc26e6f9ed306bfba30
toastsandwich <shreyasxcore2003@gmail.com>
2024-05-21 11:27:22 +0530

    called routes & corrected server struct

--- book-management-api/cmd/web/main.go
+++ book-management-api/cmd/web/main.go
@@ -9,15 +9,17 @@ import (
 
 func Start() {
 	app := LoadApplication()
-
+	app.Routes()
+	
 	server := http.Server{
+		Addr:           "localhost:8080",
 		Handler:        app.Mux,
 		ReadTimeout:    10 * time.Second,
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20, // 1 MB
 	}
 
-	fmt.Println("server on port 4000")
+	fmt.Println("server on port 8080")
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatal(err.Error())
 	}
