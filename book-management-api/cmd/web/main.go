96f72a9702e5bcef373a9cccb75cbd764914bef0
toastsandwich <shreyasxcore2003@gmail.com>
2024-05-21 07:40:00 +0530

    removed default server struct to custom server

--- book-management-api/cmd/web/main.go
+++ book-management-api/cmd/web/main.go
@@ -2,11 +2,23 @@ package web
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
 )
 
 func Start() {
 	app := LoadApplication()
+
+	server := http.Server{
+		Handler:        app.Mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20, // 1 MB
+	}
+
 	fmt.Println("server on port 4000")
-	http.ListenAndServe(":4000", app.Mux)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err.Error())
+	}
 }
